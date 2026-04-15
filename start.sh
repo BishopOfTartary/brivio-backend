cd2ab8dd868f6c32f9f4c568ee77616b7a5b4048
Sean Wiggins <wigginsbey@gmail.com>
2026-04-14 20:47:16 -0700

    remove start script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-python3 -m venv venv
-source venv/bin/activate
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
-exec uvicorn main:app --host 0.0.0.0 --port $PORT
