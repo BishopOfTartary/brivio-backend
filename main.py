3db6d3344419a89578eb15c1f7514fee8590cb23
Sean Wiggins <wigginsbey@gmail.com>
2026-04-15 13:29:07 -0700

    force correct supabase host

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 def health():
     return {"status": "online"}
 from sqlalchemy import create_engine
-DATABASE_URL = "postgresql+psycopg://postgres.rkorxkgqnavnlgirrbvp:Damiennaes7%24@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql+psycopg://postgres.rkorxkgqnavnlgirrbvp:Damiennaes7%24@aws-1-us-east-1.pooler.supabase.com:6543/postgres"pooler.supabase.com:6543/postgres"
 engine = create_engine(DATABASE_URL)
 
 @app.get("/db-test")
