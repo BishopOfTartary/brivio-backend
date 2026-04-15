2f46783f17f143467b182e07b438403e520005dc
Sean Wiggins <wigginsbey@gmail.com>
2026-04-15 12:53:06 -0700

    fix db username

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ app.add_middleware(
 def health():
     return {"status": "online"}
 from sqlalchemy import create_engine
-
 DATABASE_URL = "postgresql+psycopg://postgres.rkorxkgqnavnlgirrbvp:Damiennaes7%24@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
 engine = create_engine(DATABASE_URL)
 
