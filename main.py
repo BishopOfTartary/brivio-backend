a6c353c002eab1dcfaf91e1c0343b54c7b95f251
Sean Wiggins <wigginsbey@gmail.com>
2026-04-15 11:41:15 -0700

    fix db password encoding

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ app.add_middleware(
 @app.get("/health")
 def health():
     return {"status": "online"}
+from sqlalchemy import create_engine
+
+DATABASE_URL = "postgresql://postgres:Damiennaes7%24@db.rkorxkgqnavnlgirrbvp.supabase.co:5432/postgres"
+engine = create_engine(DATABASE_URL)
+
+@app.get("/db-test")
+def db_test():
+    try:
+        with engine.connect() as conn:
+            return {"database": "connected"}
+    except:
+        return {"database": "failed"}
